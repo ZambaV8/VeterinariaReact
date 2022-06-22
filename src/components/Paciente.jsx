83452570ddb50a4303a31ed26eff72285da264d7
ZambaV8 <v8design@hotmail.com>
2022-06-22 13:14:37 -0300

    Veterinaria

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,21 @@
+
+const Paciente = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+            <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {" "}  <span className="font-normal normal-case">El Diego</span></p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {" "}  <span className="font-normal normal-case">Juan</span></p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Email: {" "}  <span className="font-normal normal-case">correo@correo.com</span></p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {" "}  <span className="font-normal normal-case">Hook</span></p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {" "}  <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam odit qui fugit iusto quaerat natus ipsum quis voluptate, deleniti suscipit voluptatum incidunt quo veniam, dolor laudantium. Nostrum, cupiditate quia.</span></p>
+
+          
+
+          </div>
+  )
+}
+
+export default Paciente
