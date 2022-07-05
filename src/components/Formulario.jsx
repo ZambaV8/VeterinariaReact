b6f5616c927b5a7c77dbd6d4727b2124194ed2eb
ZambaV8 <v8design@hotmail.com>
2022-07-05 11:51:31 -0300

    segunda etapa

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,53 @@
+import {useState, useEffect} from "react"
 
-const Formulario = () => {
+
+
+
+const Formulario = ({pacientes,setPacientes}) => {
+      const [nombre, setNombre] = useState("")
+      const [propietario, setPropietario] = useState("")
+      const [email, setEmail] = useState("")
+      const [fecha, setFecha] = useState("")
+      const [sintomas, setSintomas] = useState("")
+
+      const [error, setError] = useState(false)
+
+
+      const handleSubmit= (e) => {
+        e.preventDefault();
+        // Validaciones del Formulario 
+ 
+        if([nombre, propietario, email, fecha, sintomas].includes("")){
+              console.log("hay al menos 1 campo vacio")
+              setError(true)
+              return; }
+              
+
+        setError(false)
+      // objeto pacientes
+
+      const objetoPaciente = {
+        nombre,
+        propietario,
+        email,
+        fecha,
+        sintomas
+
+      }
+      
+        setPacientes([...pacientes, objetoPaciente]);
+
+        // Para reiniciar el formulario
+
+        setNombre("")
+        setPropietario("")
+        setEmail("")
+        setFecha("")
+        setSintomas("")
+        
+      }
+    
+
   return (
     <div className="md:w-1/2 lg:w-2/5" >
         <h2 className="font-black text-3xl text-center">
@@ -14,7 +62,12 @@ const Formulario = () => {
 
              </p>
 
-             <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10 mt-10">
+             <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10 mt-10"
+                    onSubmit={handleSubmit}>
+                        {error && 
+                        <div className="bg-red-600 text-white text-center p-3 uppercase font-bold mb-3 rounded-md ">
+                                 <p> Todos los campos son obligatorios. </p>
+                        </div>}
 
                 <div className="mb-5">
                     <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
@@ -24,6 +77,9 @@ const Formulario = () => {
                         type="text"
                         placeholder="Nombre de la mascota"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                         value={nombre}
+                         onChange={(e) => setNombre(e.target.value)}
+
                         />
 
                 </div>
@@ -36,6 +92,8 @@ const Formulario = () => {
                         type="text"
                         placeholder="Nombre de la propietario"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={propietario}
+                         onChange={(e) => setPropietario(e.target.value)}
                         />
 
                 </div>
@@ -47,6 +105,8 @@ const Formulario = () => {
                         type="email"
                         placeholder="Email contacto del Propietario"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={email}
+                         onChange={(e) => setEmail(e.target.value)}
                         />
 
                 </div>
@@ -57,6 +117,8 @@ const Formulario = () => {
                         id="alta"
                         type="date"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={fecha}
+                         onChange={(e) => setFecha(e.target.value)}
                         />
                       </div>
                 <div className="mb-5">
@@ -65,7 +127,8 @@ const Formulario = () => {
                         id="sintomas"
                         placeholder="Describe los sintomas"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                     
+                        value={sintomas}
+                         onChange={(e) => setSintomas(e.target.value)}                     
                      />
                     
                       </div>
